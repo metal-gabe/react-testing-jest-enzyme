a40a7859d66c7a0f9c8c674d572a54534c410475
neo_andres <gfl.devo@gmail.com>
2019-09-03 00:25:31 -0700

    complete udemy course up to lesson 16

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {});
+test('renders increment button', () => {});
+test('renders counter display', () => {});
+test('counter starts at 0', () => {});
+test('clicking button increments counter display', () => {});
