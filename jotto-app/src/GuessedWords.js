64d7d870a5e8501998efafe97722b3683bf4372c
Gabriel F. Lujan <gfl.devo@gmail.com>
2019-12-01 13:44:21 -0800

    add GuessedWords component files and structure

---
+++ jotto-app/src/GuessedWords.js
@@ -0,0 +1,24 @@
+// ---------------------------------------------
+// ALL IMPORTS
+// ---------------------------------------------
+// packages
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// context
+
+// components
+
+// assets
+
+// constants
+
+// utils / methods
+
+// styles
+
+const GuessedWords = props => {};
+
+GuessedWord.propTypes = {};
+
+export default GuessedWords;
