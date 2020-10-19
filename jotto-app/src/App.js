b1cbed639d2bd86059a60d309d74e18b5c637bf8
Gabriel Arellano <anewlevelmp@gmail.com>
2020-10-18 22:29:01 -0700

    add editorconfig file;

--- jotto-app/src/App.js
+++ jotto-app/src/App.js
@@ -2,67 +2,67 @@
 /* ALL IMPORTS */
 /* -------------------------------------------------------------------------- */
 // React
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 // Packages
 // Context
 
 // Components
-import Congrats from './Congrats';
-import GuessedWords from './GuessedWords';
-import Input from './Input';
+import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
+import Input from "./Input";
 
 // Assets
-import logo from './logo.svg';
+import logo from "./logo.svg";
 // Constants
 
 // Utils / Methods
-import { getSecretWord } from './actions';
+import { getSecretWord } from "./actions";
 
 // Styles
-import './App.css';
+import "./App.css";
 
 /* -------------------------------------------------------------------------- */
 /* START OF MAIN APP COMPONENT */
 /* -------------------------------------------------------------------------- */
 export class UnconnectedApp extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {};
-  }
+		this.state = {};
+	}
 
-  /**
- * @method componentDidMount
- * @returns {undefined}
- */
-  componentDidMount() {
-    // get the secret word
-    const { getSecretWord } = this.props;
-    getSecretWord();
-  }
+	/**
+	 * @method componentDidMount
+	 * @returns {undefined}
+	 */
+	componentDidMount() {
+		// get the secret word
+		const { getSecretWord } = this.props;
+		getSecretWord();
+	}
 
-  render() {
-    const { guessedWords, success } = this.props;
+	render() {
+		const { guessedWords, success } = this.props;
 
-    return (
-      <div className="container">
-        <header className="App-header">
-          <h1 className="App-title">Jotto Game</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-          <Congrats success={success} />
-          <Input />
-          <GuessedWords guessedWords={guessedWords} />
-        </header>
-      </div>
-    );
-  }
+		return (
+			<div className="container">
+				<header className="App-header">
+					<h1 className="App-title">Jotto Game</h1>
+					<img src={logo} className="App-logo" alt="logo" />
+					<Congrats success={success} />
+					<Input />
+					<GuessedWords guessedWords={guessedWords} />
+				</header>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-  const { guessedWords, secretWord, success } = state;
-  return { guessedWords, secretWord, success };
+const mapStateToProps = (state) => {
+	const { guessedWords, secretWord, success } = state;
+	return { guessedWords, secretWord, success };
 };
 
 export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
--- jotto-app/src/App.test.js
+++ jotto-app/src/App.test.js
@@ -2,22 +2,22 @@
 // ALL IMPORTS
 // ---------------------------------------------
 // React
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Packages
-import { shallow } from 'enzyme';
+import { shallow } from "enzyme";
 
 // Context
 
 // Components
-import App, { UnconnectedApp } from './App';
+import App, { UnconnectedApp } from "./App";
 
 // Assets
 // Constants
 
 // Utils / Methods
-import { storeFactory } from '../test/testUtils';
+import { storeFactory } from "../test/testUtils";
 
 // Styles
 
@@ -33,96 +33,96 @@ import { storeFactory } from '../test/testUtils';
  * @returns {ShallowWrapper}
  */
 const setup = (state = {}) => {
-  const store = storeFactory(state);
-  const wrapper = shallow(<App store={store} />)
-    .dive()
-    .dive();
-  return wrapper;
+	const store = storeFactory(state);
+	const wrapper = shallow(<App store={store} />)
+		.dive()
+		.dive();
+	return wrapper;
 };
 
 /* -------------------------------------------------------------------------- */
 /* START OF UNIT TESTS FOR MAIN APP COMPONENT */
 /* -------------------------------------------------------------------------- */
-describe('redux properties', () => {
-  it('has access to `success` state', () => {
-    // GIVEN
-    const success = true;
-    const wrapper = setup({
-      success,
-    });
-    const { success: successProp } = wrapper.instance().props;
+describe("redux properties", () => {
+	it("has access to `success` state", () => {
+		// GIVEN
+		const success = true;
+		const wrapper = setup({
+			success,
+		});
+		const { success: successProp } = wrapper.instance().props;
 
-    // WHEN
-    // THEN
-    expect(successProp).toBe(success);
-  });
+		// WHEN
+		// THEN
+		expect(successProp).toBe(success);
+	});
 
-  it('has access to `secretWord` state', () => {
-    // GIVEN
-    const secretWord = 'party';
-    const wrapper = setup({
-      secretWord,
-    });
-    const { secretWord: secretWordProp } = wrapper.instance().props;
+	it("has access to `secretWord` state", () => {
+		// GIVEN
+		const secretWord = "party";
+		const wrapper = setup({
+			secretWord,
+		});
+		const { secretWord: secretWordProp } = wrapper.instance().props;
 
-    // WHEN
-    // THEN
-    expect(secretWordProp).toBe(secretWord);
-  });
+		// WHEN
+		// THEN
+		expect(secretWordProp).toBe(secretWord);
+	});
 
-  it('has access to `guessedWords` state', () => {
-    // GIVEN
-    const guessedWords = [
-      {
-        guessedWord: 'train',
-        letterMatchCount: 3,
-      },
-    ];
-    const wrapper = setup({
-      guessedWords,
-    });
-    const { guessedWords: guessedWordsProp } = wrapper.instance().props;
+	it("has access to `guessedWords` state", () => {
+		// GIVEN
+		const guessedWords = [
+			{
+				guessedWord: "train",
+				letterMatchCount: 3,
+			},
+		];
+		const wrapper = setup({
+			guessedWords,
+		});
+		const { guessedWords: guessedWordsProp } = wrapper.instance().props;
 
-    // WHEN
-    // THEN
-    expect(guessedWordsProp).toEqual(guessedWords);
-  });
+		// WHEN
+		// THEN
+		expect(guessedWordsProp).toEqual(guessedWords);
+	});
 
-  it('`getSecretWord` action creator is a function on the props', () => {
-    // GIVEN
-    const wrapper = setup();
-    const { getSecretWord: getSecretWordProp } = wrapper.instance().props;
+	it("`getSecretWord` action creator is a function on the props", () => {
+		// GIVEN
+		const wrapper = setup();
+		const { getSecretWord: getSecretWordProp } = wrapper.instance().props;
 
-    // WHEN
-    // THEN
-    expect(getSecretWordProp).toBeInstanceOf(Function);
-  });
+		// WHEN
+		// THEN
+		expect(getSecretWordProp).toBeInstanceOf(Function);
+	});
 });
 
-describe('initial App mount', () => {
-  it('`getSecretWord` runs on App mount', () => {
-    // GIVEN
-    const getSecretWordMock = jest.fn();
-    const testProps = {
-      getSecretWord: getSecretWordMock,
-      guessedWords: [
-        {
-          guessedWord: 'train',
-          letterMatchCount: 3,
-        },
-      ],
-      success: false,
-    };
-    // set up app component with `getSecretWordMock` as the `getSecretWord` prop
-    const wrapper = shallow(<UnconnectedApp {...testProps} />);
+describe("initial App mount", () => {
+	it("`getSecretWord` runs on App mount", () => {
+		// GIVEN
+		const getSecretWordMock = jest.fn();
+		const testProps = {
+			getSecretWord: getSecretWordMock,
+			guessedWords: [
+				{
+					guessedWord: "train",
+					letterMatchCount: 3,
+				},
+			],
+			success: false,
+		};
+		// set up app component with `getSecretWordMock` as the `getSecretWord` prop
+		const wrapper = shallow(<UnconnectedApp {...testProps} />);
 
-    // WHEN
-    // run the lifecycle method
-    wrapper.instance().componentDidMount();
-    // check to see if our mock ran
-    const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+		// WHEN
+		// run the lifecycle method
+		wrapper.instance().componentDidMount();
+		// check to see if our mock ran
+		const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
 
-    // THEN
-    expect(getSecretWordCallCount).toBe(1);
-  });
+		// THEN
+		expect(getSecretWordCallCount).toBe(1);
+	});
 });
