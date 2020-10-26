afd45525995fca59862e6bee05ae0f538aaf9a67
Gabriel Arellano <anewlevelmp@gmail.com>
2020-10-25 22:31:48 -0700

    add initial hooks/context test & setup;

--- base-for-hooks-context/src/App.test.js
+++ base-for-hooks-context/src/App.test.js
@@ -1,9 +1,46 @@
+/* ========================================================================== */
+// ALL REQUIRED IMPORTS
+/* ========================================================================== */
+// React
 import React from "react";
-import { render } from "@testing-library/react";
+
+// Packages
+import { shallow } from "enzyme";
+
+// Context
+
+// Components
 import App from "./App";
 
-test("renders learn react link", () => {
-	const { getByText } = render(<App />);
-	const linkElement = getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+// Assets
+// Constants
+
+// Utils / Methods
+import { findByTestAttr } from "../test/testUtils";
+
+// Styles
+
+/* ========================================================================== */
+// SETUP UTIL AND HELPER METHODS
+/* ========================================================================== */
+/**
+ * Setup function for App component.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+	return shallow(<App {...props} />);
+};
+
+/* ========================================================================== */
+// START OF ALL UNIT TESTS FOR MAIN APP COMPONENT
+/* ========================================================================== */
+it("App renders without error", () => {
+	// GIVEN
+	const wrapper = setup();
+
+	// WHEN
+	const component = findByTestAttr(wrapper, "component-app");
+
+	// THEN
+	expect(component.length).toBe(1);
 });
