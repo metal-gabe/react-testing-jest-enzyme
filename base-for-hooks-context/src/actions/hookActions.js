e5b0c205e54556f9cb27ff464f8fd76a485df0fb
Gabriel Arellano <anewlevelmp@gmail.com>
2020-11-02 21:52:47 -0800

    install Axios & Moxios packages; add file for `hookActions` tests;

---
+++ base-for-hooks-context/src/actions/hookActions.js
@@ -0,0 +1,23 @@
+/* ========================================================================== */
+// ALL REQUIRED IMPORTS
+/* ========================================================================== */
+// React
+
+// Packages
+import axios from "axios";
+
+// Context
+// Components
+// Assets
+// Constants
+// Utils / Methods
+// Styles
+
+/* ========================================================================== */
+// START OF ALL HOOK ACTIONS DEFINITIONS
+/* ========================================================================== */
+export const getSecretWord = (setSecretWord) => {};
+
+export default {
+	getSecretWord,
+};
